5b171d870d57c6e4a2a28d02e5b489ea2b3f9222
Ramiro Rodrigo <gatto619@gmail.com>
2021-03-03 22:32:39 -0400

    arreglo

--- myDrawer/index.js
+++ myDrawer/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { View } from 'react-native';
-import { Text , TouchableOpacity, Button} from 'react-native';
+import { Text , TouchableOpacity, Button, Image, StyleSheet} from 'react-native';
 
 import { createDrawerNavigator } from '@react-navigation/drawer';
 //const Stack = createStackNavigator();
@@ -14,7 +14,13 @@ function MyDrawer() {
         <NavigationContainer>
             <Drawer.Navigator>
                 <Drawer.Screen name="Home" component={HomeScreen} />
+                <Drawer.Screen name="image" component={imageScreen} />
                 <Drawer.Screen name="Profile" component={ProfileScreen} />
+                <Drawer.Screen name="Mylisten" component={MylistenScreen} />
+                <Drawer.Screen name="Forums" component={ForumsScreen} />
+                <Drawer.Screen name="Setting" component={SettingScreen} />
+                <Drawer.Screen name="Help" component={HelpScreen} />
+                <Drawer.Screen name="Logout" component={LogoutScreen} />
             </Drawer.Navigator>
         </NavigationContainer>
         
@@ -34,6 +40,16 @@ function MyDrawer() {
     </NavigationContainer>
   );
 };*/
+function imageScreen(navigation, route) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{fontSize:30}}>This is a banana!!!
+            </Text>
+            <Image styles ={styles.imgStyle}
+            source = {require('../src/images/images.png')}/>
+    </View>
+    );
+  };
 
 function HomeScreen(props) {
     return (
@@ -49,7 +65,31 @@ function HomeScreen(props) {
     );
   };
 function ProfileScreen(navigation, route) {
-    return <Text>This is Ramiro's profile</Text>;
+    return <Text style={{fontSize:30}}>This is Ramiro's profile</Text>;
   };
+
+  function MylistenScreen(navigation, route) {
+    return <Text style={{fontSize:30}}>This is my listenings</Text>;
+  };
+  function ForumsScreen(navigation, route) {
+    return <Text style={{fontSize:30}}>This is my forums</Text>;
+  };
+  function SettingScreen(navigation, route) {
+    return <Text style={{fontSize:30}}>This is the settings</Text>;
+  };
+  function HelpScreen(navigation, route) {
+    return <Text>you need help?</Text>;
+  };
+  function LogoutScreen(navigation, route) {
+    return <Text style={{fontSize:30}}>Can't log out</Text>;
+  };
+
+  const styles = StyleSheet.create({
+      imgStyle: {
+          width: 100,
+          height: 100,
+          marginTop: 30
+      }
+  })
 
   export default MyDrawer
